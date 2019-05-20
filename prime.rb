08841ffe21d5c6d0889411d6a385237f819af5e8
Warren Yao <warrenyca905@gmail.com>
2019-05-20 16:45:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  i = 2
+  if num == 2 || num == 3
+    return true
+  elsif num <= 1
+    return false
+  else
+    while i < num
+      if num % i == 0
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+end
