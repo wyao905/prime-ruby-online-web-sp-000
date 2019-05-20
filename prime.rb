927d8a3a73877d7b0a00f910cf69dc9453ae0317
Warren Yao <warrenyca905@gmail.com>
2019-05-20 15:58:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,8 @@
 # Add  code here!
 def prime?(num)
+  collect = []
   if (num + 1) % 6 == 0 || (num - 1) % 6 == 0
-    
+    collect << num
+    collect.each do |test_num|
+      if num % (test_num * test_num) == 0
+        
