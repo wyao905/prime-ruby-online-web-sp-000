774463609cd547b2b62dabf6ae8db9517b3dce25
Warren Yao <warrenyca905@gmail.com>
2019-05-20 16:04:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 def prime?(num)
+  i = 0
   collect = []
-  if (num + 1) % 6 == 0 || (num - 1) % 6 == 0
-    collect << num
-    collect.each do |test_num|
-      if num % (test_num * test_num) == 0
-        collect.delete(test_num)
+  while i < num
+    if (num + 1) % 6 == 0 || (num - 1) % 6 == 0
+      collect.each do |test_num|
+        if num % (test_num * test_num) == 0
+          collect.delete(test_num)
+        end
