5c360f035106cf8c75d785008da87904bece6661
Warren Yao <warrenyca905@gmail.com>
2019-05-20 16:38:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,15 @@
 # Add  code here!
 def prime?(num)
   i = 4
-  collect = []
   if num == 2 || num == 3
     true
-  elsif num > 1
-    if ((num + 1) % 6 == 0 || (num - 1) % 6 == 0) && (num % 5 != 0)
-      while i < num
-        if num % i == 0
-          false
-        end
-        i += 1
-      end
-      true
-    else
-      false
-    end
   else
-    false
+    while i < num
+      if num % i == 0
+        false
+      end
+      i += 1
+    end
+    true
   end
 end
