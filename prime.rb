ba1131cb0d9656e3df4d04361f619c6079003789
Warren Yao <warrenyca905@gmail.com>
2019-05-20 16:18:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 # Add  code here!
 def prime?(num)
   if num != 1
-    if (num + 1) % 6 == 0 || (num - 1) % 6 == 0
+    if ((num + 1) % 6 == 0 || (num - 1) % 6 == 0) && (num % 5 != 0)
       while i < num
-        if num % (i * i) != 0
+        if num % (i * i) == 0
+          false
+        end
+      end
     else
       false
     end
